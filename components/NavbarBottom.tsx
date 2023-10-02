843e139c7da71f26873b50ec8b6f5dc7ab02429d
Ben Nguyen <bennguyenn24@gmail.com>
2023-10-02 00:08:40 -0700

    Added and revised logo sizing and extras

--- components/NavbarBottom.tsx
+++ components/NavbarBottom.tsx
@@ -7,19 +7,31 @@ import { phoneImg } from "../public/assets/images";
 const NavbarBottom = () => {
     return (
         <div className="max-w-container mx-auto py-2 px-6 flex items-center justify-between">
-            <div>
-                <div className="">
+            <div className="flex items-center gap-4">
+                <div className="flex items-center gap-2">
                     <Image
                         src={phoneImg}
-                        width={50}
+                        width={10}
                         height={50}
                         alt="Phone Img"
                         className="bg-blue-500 w-6"
                     />
                     <p>Give us a call!</p>
+                    <FiChevronDown />
+                    <span className="w-[1px] h-4 bg-white inline-flex ml-2"></span>
                 </div>
-                <div></div>
+                <div className="flex items-center gap-2">
+                    <MdOutlineLocationOn />
+                    <p className="text-xs">Santa Ana, 92704</p>
+                    <FaPlaceOfWorship />
+                    <p className="text-xs">Santa Ana Supercenter</p>
+                </div>
             </div>
+            <ul className="flex items-center gap-4">
+                <li className="subNavbarHover">Deals</li>
+                <li className="subNavbarHover">About</li>
+                <li className="subNav">Home</li>
+            </ul>
         </div>
     );
 };
