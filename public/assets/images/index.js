f95160315bbe200813eb402a1561e954f51fa98a
Ben Nguyen <bennguyenn24@gmail.com>
2023-10-02 20:49:14 -0700

    Updated Product page

--- public/assets/images/index.js
+++ public/assets/images/index.js
@@ -1,11 +1,11 @@
 import logo from "./logo.png";
 import phoneImg from "./phoneImg.jpg"
-import sliderImgOne from "./slider/sliderImgOne.webp";
-import sliderImgTwo from "./slider/sliderImgTwo.webp";
-import sliderImgThree from "./slider/sliderImgThree.webp";
-import sliderImgFour from "./slider/sliderImgFour.webp";
-import sliderImgFive from "./slider/sliderImgFive.webp";
-import bannerImg from "./slider/bannerImg.webp";
+import sliderImgOne from "./slider/sliderImgOne.jpg"
+import sliderImgTwo from "./slider/sliderImgTwo.jpg"
+import sliderImgThree from "./slider/sliderImgThree.jpg"
+import sliderImgFour from "./slider/sliderImgFour.jpg"
+import sliderImgFive from "./slider/sliderImgFive.jpg"
+import heroImg from "./slider/heroImg.jpg"
 import ship1Img from "./ship/ship1.png";
 import ship2Img from "./ship/ship2.png";
 import ship3Img from "./ship/ship3.png";
@@ -21,7 +21,7 @@ export {
   sliderImgThree,
   sliderImgFour,
   sliderImgFive,
-  bannerImg,
+  heroImg,
   ship1Img,
   ship2Img,
   ship3Img,
