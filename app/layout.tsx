acc45c1654d6361df9a1d011fc206b70620df6aa
Ben Nguyen <bennguyenn24@gmail.com>
2023-10-06 16:15:16 -0700

    Fixed bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,30 @@
-import './globals.css'
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Navbar from '@/components/Navbar'
-import HeroSection from '@/components/HeroSection';
+import "./globals.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Steakhouse Society',
-}
+    title: "Steakhouse Society",
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <>
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-      <main className='herosection'>
-      <Navbar />
-        <div className='max-w-contentContainer mx-auto bg-white'>
-        <HeroSection />
-        </div>
-      </main>
-    </html>
-    </>
-  )
+    return (
+        <>
+            <html lang="en">
+                <body className={inter.className}>
+                    <Navbar />
+                    {children}
+                </body>
+            </html>
+        </>
+    );
 }
