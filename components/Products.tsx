a4b5eae367476c0e0fbe64a60000a5b55b70abf5
Ben Nguyen <bennguyenn24@gmail.com>
2023-10-13 09:55:41 -0700

    Edited tailwind CSS

--- components/Products.tsx
+++ components/Products.tsx
@@ -11,22 +11,22 @@ const Products = ({ products }: ProductsProps) => {
             <div className="flex col-span-4">
                 <span
                     className="w-full h-full pt-6 pb-6 bg-gray-900 text-white flex text-center justify-center font-bold hover:text-blue-900 p-4 rounded-sm"
-                    href="/collection"
                 >
                     CHECKOUT OUR COLLECTION
                 </span>
             </div>
-            <div className="py-10 px-10 grid grid-cols-4 gap-4">
+
+            <div className="py-24 px-10 grid grid-cols-4 gap-4">
                 {products.map((product: Product) => (
                     <div
                         key={product.id}
                         className="border-gray-600 mb-6 group shadow-xl"
                     >
-                        <div className="w-full h-full pr-4 object-contain">
+                        <div className="w-max-lg h-64  pr-4 object-cover">
                             <Image
-                                width={150}
-                                height={200}
-                                className="w-40 h-52 object-contain"
+                                width={300}
+                                height={250}
+                                className="w-full h-80 object-contain"
                                 src={product.img}
                                 alt="itemimage"
                             />
@@ -35,7 +35,7 @@ const Products = ({ products }: ProductsProps) => {
                             </h2>
                         </div>
                         <div className="px-2 py-6 flex flex-col justify-center">
-                            <div className="flex justify-between py-2 px-2">
+                            <div className="flex justify-between py-2 px-4">
                                 <button className="w-20 h-9 bg-gray-900 text-white rounded-full flex gap-1 items-center justify-center hover:bg-gray-600 duration-300">
                                     +
                                 </button>
@@ -43,7 +43,7 @@ const Products = ({ products }: ProductsProps) => {
                                     Details
                                 </button>
                             </div>
-                            <p className="text-white text-lg font-semibold">
+                            <p className="text-black m-5 pt-4 text-lg font-semibold">
                                 Price: ${product.price}
                             </p>
                         </div>
