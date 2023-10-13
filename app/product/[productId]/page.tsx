c266a447a00d978bafa67c921749c82f064d8e59
Ben Nguyen <bennguyenn24@gmail.com>
2023-10-13 13:04:51 -0700

    Linked individual product page

---
+++ app/product/[productId]/page.tsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import Image from "next/image";
+
+export default async function ProductPage({
+    params,
+}: {
+    params: { productId: string };
+}) {
+    const { productId } = params;
+
+    const { data: product } = await axios.get(
+        `${process.env.HOST_URL}/api/products/${productId}`
+    );
+
+    if (!product) {
+        throw new Error("Product failed to load.");
+    }
+
+    return (
+        <div className="flex w-full h-full gap-4 justify-between items-center md:px-[12%] md:py-[4%]">
+            <div className="relative w-[800px] h-[400px] ">
+                <Image
+                    className="scale-100 group-hover:scale-105 duration-300 object-cover rounded-md"
+                    src={product.img}
+                    alt={product.name}
+                    fill
+                />
+            </div>
+
+            <div>
+                <h2 className="font-semibold">{product.name}</h2>
+                <p>{product.description}</p>
+
+                <p>${product.price}</p>
+            </div>
+        </div>
+    );
+}
--- components/Products.tsx
+++ components/Products.tsx
@@ -6,12 +6,11 @@ interface ProductsProps {
 }
 
 const Products = ({ products }: ProductsProps) => {
+    console.log(products[0]);
     return (
         <div>
             <div className="flex col-span-4">
-                <span
-                    className="w-full h-full pt-6 pb-6 bg-gray-900 text-white flex text-center justify-center font-bold hover:text-blue-900 p-4 rounded-sm"
-                >
+                <span className="w-full h-full pt-6 pb-6 bg-gray-900 text-white flex text-center justify-center font-bold hover:text-blue-900 p-4 rounded-sm">
                     CHECKOUT OUR COLLECTION
                 </span>
             </div>
@@ -20,21 +19,23 @@ const Products = ({ products }: ProductsProps) => {
                 {products.map((product: Product) => (
                     <div
                         key={product.id}
-                        className="border-gray-600 mb-6 group shadow-xl"
+                        className="border-gray-600 group shadow-xl"
                     >
-                        <div className="w-max-lg h-64  pr-4 object-cover">
-                            <Image
-                                width={300}
-                                height={250}
-                                className="w-full h-80 object-contain"
-                                src={product.img}
-                                alt="itemimage"
-                            />
-                            <h2 className="text-xl text-center font-semibold pt-2 mb-2">
-                                {product.name}
-                            </h2>
+                        <div className="relative w-64 h-64">
+                            <Link href={`/product/${product.id}`}>
+                                <Image
+                                    className="scale-100 group-hover:scale-105 duration-300"
+                                    layout="fill"
+                                    objectFit="cover"
+                                    src={product.img}
+                                    alt={product.name}
+                                />
+                            </Link>
                         </div>
                         <div className="px-2 py-6 flex flex-col justify-center">
+                            <h2 className="text-xl text-center font-semibold">
+                                {product.name}
+                            </h2>
                             <div className="flex justify-between py-2 px-4">
                                 <button className="w-20 h-9 bg-gray-900 text-white rounded-full flex gap-1 items-center justify-center hover:bg-gray-600 duration-300">
                                     +
