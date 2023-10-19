8616d717fcd385860d2b1dd25f5e33eb96604c58
Ben Nguyen <bennguyenn24@gmail.com>
2023-10-18 21:47:30 -0700

    Revised Icons

--- public/assets/images/index.js
+++ public/assets/images/index.js
@@ -6,9 +6,6 @@ import sliderImgThree from "./slider/sliderImgThree.jpg"
 import sliderImgFour from "./slider/sliderImgFour.jpg"
 import sliderImgFive from "./slider/sliderImgFive.jpg"
 import heroImg from "./slider/heroImg.jpg"
-import ship1Img from "./ship/ship1.png";
-import ship2Img from "./ship/ship2.png";
-import ship3Img from "./ship/ship3.png";
 import notFoundImg from "./404Img.png";
 import emptyCart from "./empty-cart.png";
 import warningImg from "./warning.png";
@@ -22,9 +19,6 @@ export {
   sliderImgFour,
   sliderImgFive,
   heroImg,
-  ship1Img,
-  ship2Img,
-  ship3Img,
   notFoundImg,
   emptyCart,
   warningImg,
