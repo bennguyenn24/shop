89f4e8b3f2a5f59971b2ce1816915aa5222e3395
Ben Nguyen <bennguyenn24@gmail.com>
2023-10-19 21:36:41 -0700

    fixed footer on cart page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+    return <div className="h-[calc(100vh-140px)]">page</div>;
+};
 
-export default page
+export default page;
