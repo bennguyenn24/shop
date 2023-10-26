c4ffe2e91044e113abb026aaaae968f53fd17647
Ben Nguyen <bennguyenn24@gmail.com>
2023-10-25 22:13:28 -0700

    Added Popover

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { IoMdHeartEmpty } from "react-icons/io";
 import { BsStarFill, BsInfoCircle } from "react-icons/bs";
 import { FaShippingFast } from "react-icons/fa";
+import { useState } from "react";
 
 export default async function ProductPage({
     params,
@@ -54,32 +55,109 @@ export default async function ProductPage({
                     </div>
                     {/* Product Info  */}
                     <div>
-                        <h2 className="font-xl font-semibold underline underline-offset-4">{product.name}</h2>
-                        <p>
-                            {product.description}
-                        </p>
+                        <h2 className="font-xl font-semibold underline underline-offset-4">
+                            {product.name}
+                        </h2>
+                        <p>{product.description}</p>
+                        <div className="flex gap-2 items-center text-xs mt-2">
+                            <div className="flex gap-1">
+                                <BsStarFill />
+                                <BsStarFill />
+                                <BsStarFill />
+                                <BsStarFill />
+                                <BsStarFill />
+                            </div>
+                            <p>50</p>
+                        </div>
+                        <div className="flex items-end gap-2">
+                            <p className="pt-2 font-semibold">
+                                ${product.price} 1x100g
+                            </p>
+                            <div>
+                                <p className="flex items-center text-sm text-gray-500 dark:text-gray-400">
+                                    This is just some informational text{" "}
+                                    <button
+                                        data-popover-target="popover-description"
+                                        data-popover-placement="bottom-end"
+                                        type="button"
+                                    >
+                                        <svg
+                                            className="w-4 h-4 ml-2 text-gray-400 hover:text-gray-500"
+                                            aria-hidden="true"
+                                            fill="currentColor"
+                                            viewBox="0 0 20 20"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path
+                                                fill-rule="evenodd"
+                                                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                                                clip-rule="evenodd"
+                                            ></path>
+                                        </svg>
+                                        <span className="sr-only">
+                                            Show information
+                                        </span>
+                                    </button>
+                                </p>
+                                <div
+                                    data-popover
+                                    id="popover-description"
+                                    role="tooltip"
+                                    className="absolute z-10 invisible inline-block text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 w-72 dark:bg-gray-800 dark:border-gray-600 dark:text-gray-400"
+                                >
+                                    <div className="p-3 space-y-2">
+                                        <h3 className="font-semibold text-gray-900 dark:text-white">
+                                            Activity growth - Incremental
+                                        </h3>
+                                        <p>
+                                            Report helps navigate cumulative
+                                            growth of community activities.
+                                            Ideally, the chart should have a
+                                            growing trend, as stagnating chart
+                                            signifies a significant decrease of
+                                            community activity.
+                                        </p>
+                                        <h3 className="font-semibold text-gray-900 dark:text-white">
+                                            Calculation
+                                        </h3>
+                                        <p>
+                                            For each date bucket, the all-time
+                                            volume of activities is calculated.
+                                            This means that activities in period
+                                            n contain all activities up to
+                                            period n, plus the activities
+                                            generated by your community in
+                                            period.
+                                        </p>
+                                        <a
+                                            href="#"
+                                            className="flex items-center font-medium text-blue-600 dark:text-blue-500 dark:hover:text-blue-600 hover:text-blue-700 hover:underline"
+                                        >
+                                            Read more{" "}
+                                            <svg
+                                                className="w-2 h-2 ml-1.5"
+                                                aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="none"
+                                                viewBox="0 0 6 10"
+                                            >
+                                                <path
+                                                    stroke="currentColor"
+                                                    stroke-linecap="round"
+                                                    stroke-linejoin="round"
+                                                    stroke-width="2"
+                                                    d="m1 9 4-4-4-4"
+                                                />
+                                            </svg>
+                                        </a>
+                                    </div>
+                                    <div data-popper-arrow></div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     );
-}
-
-{
-    /* <div className="flex w-full h-full gap-4 justify-between items-center md:px-[12%] md:py-[4%]">
-<div className="relative w-[800px] h-[400px] ">
-    <Image
-        className="scale-100 group-hover:scale-105 duration-300 object-cover rounded-md"
-        src={product.img}
-        alt={product.name}
-        fill
-    />
-</div>
-
-<div>
-    <h2 className="font-semibold">{product.name}</h2>
-    <p>{product.description}</p>
-    <p>${product.price}</p>
-</div>
-</div> */
 }
