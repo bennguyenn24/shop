50b0e1a917d75b09cb22ff4181ede4cb7174def9
Ben Nguyen <bennguyenn24@gmail.com>
2023-11-22 19:46:38 -0800

    Need to fix cart page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,14 +1,30 @@
 import React from "react";
 
 const page = () => {
-    return <div className="w-full py-10">
-        <div className="w-full flex gap-10">
-            <div className="w-2/3 flex flex-col gap-5">
-                <h1 className="semibold ">Cart</h1>
+    return (
+        <div>
+            <div className="flex items-center">
+                <div className="border-b border-gray-400 flex-grow"></div>
+                <span className="mx-3 text-gray-500">Cart</span>
+                <div className="border-b border-gray-400 flex-grow"></div>
             </div>
-            <div className="w-1/3 p-4 mt-24 h-[500px] border-[1px] border-gray-900 rounded-md justify-center flex flex-col gap-5">Philip</div>
-        </div>
-    </div>;
+            <div className="flex">
+                    <div className="w-2/3 px-8 py-12 max-w-lg mx-auto">
+                        <h1 className="text-2xl font-bold mb-8">
+                            Cart
+                            <span className="text-lightText font-normal">
+                                Hello
+                            </span>
+                        </h1>
+                        {/* Cart Products Go here */}
+                        <div className=""></div>
+                    </div>
+                    <div className="  w-1/3 px-8 py-12 mt-22 border-[1px] border-gray-900 rounded-md">
+                        Philip
+                    </div>
+                </div>
+            </div>
+    );
 };
 
 export default page;
