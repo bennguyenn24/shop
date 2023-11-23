8efab738c62d6db043b77ee7c6e0f89b4f5007d8
Ben Nguyen <bennguyenn24@gmail.com>
2023-11-22 21:18:14 -0800

    Rebranded Cart Page to make it look nicer

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,29 +1,17 @@
 import React from "react";
+import Link from "next/link";
 
 const page = () => {
     return (
-        <div>
-            <div className="flex items-center">
-                <div className="border-b border-gray-400 flex-grow"></div>
-                <span className="mx-3 text-gray-500">Cart</span>
-                <div className="border-b border-gray-400 flex-grow"></div>
+        <div className="w-full px-2 py-2 flex flex-col items-center">
+            <h1 className="text-lg font-extrabold py-2 mb-2 border-b border-gray-600">
+                Shopping Cart
+            </h1>
+            <div className=" w-full h-full flex md:flex-row flex-col ">
+                <div className=":w-8/12 w-full py-4 flex flex-wrap">Here are your items</div>
+                <div id="cart" className="lg:w-4/12 w-full py-4 px-2 text-bold">Order Summary</div>
             </div>
-            <div className="flex">
-                    <div className="w-2/3 px-8 py-12 max-w-lg mx-auto">
-                        <h1 className="text-2xl font-bold mb-8">
-                            Cart
-                            <span className="text-lightText font-normal">
-                                Hello
-                            </span>
-                        </h1>
-                        {/* Cart Products Go here */}
-                        <div className=""></div>
-                    </div>
-                    <div className="  w-1/3 px-8 py-12 mt-22 border-[1px] border-gray-900 rounded-md">
-                        Philip
-                    </div>
-                </div>
-            </div>
+        </div>
     );
 };
 
