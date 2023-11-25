925ac024ea8ec2be7513fb9f7f7a995fba855459
Ben Nguyen <bennguyenn24@gmail.com>
2023-11-24 16:30:04 -0800

    Updated Added to Cart Btn

--- components/CartDisplay.tsx
+++ components/CartDisplay.tsx
@@ -2,11 +2,12 @@
 
 import { useCart } from "@/contexts/CartContext";
 import Link from "next/link";
-import React, { useContext, useEffect, useState } from "react";
+import React from "react";
 import { BsCart2 } from "react-icons/bs";
 
 const CartDisplay = () => {
     const { cart } = useCart() as CartContextProps;
+    console.log(cart)
     const cartLength = cart.length;
     const cartTotal = cart.reduce((acc, item: Product) => acc + item.price, 0)
 
@@ -15,7 +16,7 @@ const CartDisplay = () => {
             <div className="flex flex-col justify-center items-center mt-2 gap-2 h-12 px-5 rounded-full bg-transparent hover:bg-gray-600 duration-300 relative">
                 <BsCart2 className="text-2xl" />
                 <p>${cartTotal}.00</p>
-                <span className="absolute w-4 h-4 bg-blue text-black top-0 right-3 rounded-full flex items-center justify-center text-xs">
+                <span className="absolute w-4 h-4 bg-blue text-white top-0 right-3 rounded-full flex items-center justify-center text-xs">
                     {cartLength}
                 </span>
             </div>
