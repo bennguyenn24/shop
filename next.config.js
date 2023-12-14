544e7219b512b5800ef7c9f4faa9a0522d343178
Ben Nguyen <bennguyenn24@gmail.com>
2023-12-13 21:45:39 -0800

    Created Stripe connection endpoint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: { domains: ["res.cloudinary.com"] },
+    env:{
+        stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
 };
 
 module.exports = nextConfig;
+
