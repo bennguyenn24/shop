4e34b14ac26240bc22525716dad96333094485ef
Ben Nguyen <bennguyenn24@gmail.com>
2023-12-14 16:24:18 -0800

    Stripe functionality with cart

--- app/api/checkout_sessions/route.ts
+++ app/api/checkout_sessions/route.ts
@@ -4,17 +4,15 @@ import { NextResponse } from "next/server";
 const DOMAIN_URL = process.env.HOST_URL;
 
 export async function POST(request: NextRequest) {
-    const body = await request.json();
+    const cartItems = await request.json();
 
-    // Map through your cart items and return an array of objects containing priceId and quantity
+    const modifiedItems = cartItems.map((item: { stripePriceId: any; quantity: any; }) => ({
+        price: item.stripePriceId,
+        quantity: item.quantity,
+    }));
 
     const session = await stripe.checkout.sessions.create({
-        line_items: [
-            {
-                price: "price_1Nf7vDAbUXjP2ReYleu5QF62",
-                quantity: 1,
-            },
-        ],
+        line_items: modifiedItems,
         mode: "payment",
         success_url: `${DOMAIN_URL}?success=true`,
         cancel_url: `${DOMAIN_URL}?canceled=true`,
