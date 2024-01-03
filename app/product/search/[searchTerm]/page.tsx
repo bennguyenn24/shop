dcea8a876acd1cb3e5b667451ee07bbd077024b4
Ben Nguyen <bennguyenn24@gmail.com>
2024-01-03 12:55:06 -0800

    Added responsiveness to search page

--- app/product/search/[searchTerm]/page.tsx
+++ app/product/search/[searchTerm]/page.tsx
@@ -3,6 +3,7 @@ import { getProducts } from "@/helpers/getProducts";
 import Link from "next/link";
 import AddToCartButton from "@/components/AddToCartButton";
 
+
 export default async function SearchPage({
     params,
 }: {
@@ -20,16 +21,16 @@ export default async function SearchPage({
 
     return (
         <div>
-            <h2 className="px-10 pt-4">
+            <h2 className="px-10 pt-4 text-center font-semibold mb-6">
                 Products matching with search term: {params.searchTerm}
             </h2>
-            <div className="py-24 px-10 grid grid-cols-4 gap-4">
+            <div className="grid grid-cols-1 sm:flex-col md:flex-col lg:grid-cols-4 gap-4">
                 {matchedProducts.map((product: Product) => (
                     <div
                         key={product.id}
-                        className="border-gray-600 group shadow-xl"
+                        className="max-w-md mx-auto bg-white shadow-md overflow-hidden md:max-w-2xl my-4"
                     >
-                        <div className="relative w-64 h-64">
+                        <div className="relative w-76 h-64 sm:flex">
                             <Link href={`/product/${product.id}`}>
                                 <Image
                                     className="scale-100 group-hover:scale-105 duration-300"
@@ -44,13 +45,14 @@ export default async function SearchPage({
                             <h2 className="text-xl text-center font-semibold">
                                 {product.name}
                             </h2>
+
                             <div className="flex justify-between py-2 px-4">
                                 <AddToCartButton
                                     title={"Add To Cart"}
                                     product={product}
-                                    className="rounded-full w-32 h-12 text-white bg-gray-900 hover:bg-gray-400"
+                                    className="rounded-full w-32 h-12 sm:h-12 text-white bg-gray-900 hover:bg-gray-400"
                                 />
-                                <button className="w-24 h-9 bg-white text-black border-black border-[1px] rounded-full flex gap-1 items-center justify-center hover:bg-gray-600 duration-300">
+                                <button className="w-24 h-9 sm:h-12 bg-white text-black border-black border-[1px] rounded-full flex gap-1 items-center justify-center hover:bg-gray-600 duration-300">
                                     Details
                                 </button>
                             </div>
