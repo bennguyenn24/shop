0aa03f891a88a8a3eac70cfee900467c805cd7dc
Ben Nguyen <bennguyenn24@gmail.com>
2024-01-25 12:35:08 -0800

    Removed Updated Code

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,32 +1,32 @@
-import { prisma } from "@/db/script";
-import { NextRequest, NextResponse } from "next/server"
-import { ChangeEvent, useState } from "react";
+// import { prisma } from "@/db/script";
+// import { NextRequest, NextResponse } from "next/server"
+// import { ChangeEvent, useState } from "react";
 
-export async function POST(request: NextRequest) {
-    const {query} = await request.query;
+// export async function POST(request: NextRequest) {
+//     const {query} = await request.query;
 
-    const results = await prisma.product.findMany({
-        where: {
-            name: {
-                contains: query,
-                mode: 'insensitive'
-            }
-        }
-    });
-    return NextResponse.json(results, { status: 201 });
-}
+//     const results = await prisma.product.findMany({
+//         where: {
+//             name: {
+//                 contains: query,
+//                 mode: 'insensitive'
+//             }
+//         }
+//     });
+//     return NextResponse.json(results, { status: 201 });
+// }
 
-const [searchTerm, setSearchTerm] = useState("");
-const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
+// const [searchTerm, setSearchTerm] = useState("");
+// const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
 
-const handleChangeSearch = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(e.target.value);
-    setFilteredProducts(
-        products.filter((product) =>
-            product.name.toLowerCase().includes(searchTerm.toLowerCase())
-        )
-    )};
+// const handleChangeSearch = (e: ChangeEvent<HTMLInputElement>) => {
+//     setSearchTerm(e.target.value);
+//     setFilteredProducts(
+//         products.filter((product) =>
+//             product.name.toLowerCase().includes(searchTerm.toLowerCase())
+//         )
+//     )};
 
-    axios.get(`/search?q=${searchTerm}`).then((res) => {
-    setFilteredProducts(res.data);
-    });
+//     axios.get(`/search?q=${searchTerm}`).then((res) => {
+//     setFilteredProducts(res.data);
+//     });
